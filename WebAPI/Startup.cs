8461f02555eaf47c4b656e94e9068c5e9bda8f6a
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2017-11-14 11:50:16 +0900

    AngularAppをWebAPIに参照させるよう変更

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -34,7 +34,12 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                routes.MapSpaFallbackRoute("spa-fallback", new { controller = "Home", action = "Index" });
+            });
         }
     }
 }
